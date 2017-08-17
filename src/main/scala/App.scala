378f92fc2b29770bcc5125c35ad1bc304c7ee21a
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-17 11:56:00 +0200

    Declares x as winner by default

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,10 +1,11 @@
 object App {
   var board = Array(Array("x", " ", "o"),
-    Array("x", "x", " "),
+    Array("x", "x", "x"),
     Array(" ", "o", "o"))
 
   def main(args: Array[String]) {
     printBoard()
+    println("x is the winner")
   }
 
   private def printBoard(): Unit = {
