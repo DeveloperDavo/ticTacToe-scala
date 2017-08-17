e1305dc2d893f6cd598a9de75a157860ca3433a4
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-17 13:42:03 +0200

    Extract X, O and EMPTY constants

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,7 +1,11 @@
 object App {
-  var board = Array(Array("x", "x", "o"),
-    Array("x", " ", "x"),
-    Array("o", "o", "o"))
+  private val X = "x"
+  private val O = "o"
+  private val EMPTY = " "
+
+  private var board = Array(Array(X, X, O),
+    Array(X, EMPTY, X),
+    Array(O, O, O))
 
   def main(args: Array[String]) {
     printBoard()
@@ -11,7 +15,7 @@ object App {
   private def printWinner() {
     var winner = ""
     for (row <- board.indices) {
-      if ((board(row) contains "x") || (board(row) contains "o")) {
+      if ((board(row) contains X) || (board(row) contains O)) {
         if (board(row)(0).equals(board(row)(1)) && board(row)(0).equals(board(row)(2))) {
           winner = board(row)(0)
         }
