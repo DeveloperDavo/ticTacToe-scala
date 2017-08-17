c4152a7765dd5243b6facea5a39349e32c639f51
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-17 11:48:37 +0200

    Extract printBoard, printRow, printEntry

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,14 +1,27 @@
 object App {
+  var board = Array(Array("x", " ", "o"),
+    Array("x", "x", " "),
+    Array(" ", "o", "o"))
+
   def main(args: Array[String]) {
-    var board = Array(Array("x", " ", "o"),
-      Array("x", "x", " "),
-      Array(" ", "o", "o"))
+    printBoard()
+  }
+
+  private def printBoard(): Unit = {
     board.foreach { boardRow =>
-      print("|")
-      boardRow.foreach { boardElem =>
-        print(boardElem + "|")
-      }
-      println()
+      printRow(boardRow)
     }
+  }
+
+  private def printRow(boardRow: Array[String]): Unit = {
+    print("|")
+    boardRow.foreach { boardElem =>
+      printEntry(boardElem)
+    }
+    println()
+  }
+
+  private def printEntry(boardElem: String): Unit = {
+    print(boardElem + "|")
   }
 }
