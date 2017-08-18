7638f2422e14c43daf40b098c2aee1c8abf74008
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-18 09:31:59 +0200

    Use Chars instead of Strings for board values

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,12 +1,12 @@
 object App {
-  private val X = "x"
-  private val O = "o"
-  private val E = " "
+  private val X = 'x'
+  private val O = 'o'
+  private val E = ' '
 
   private var board = Array(
-    Array(X, X, O),
+    Array(X, X, X),
     Array(O, O, X),
-    Array(X, X, O))
+    Array(X, O, O))
 
   def main(args: Array[String]) {
     printBoard()
@@ -16,14 +16,14 @@ object App {
 
   private def printTieOrWinner(): Unit = {
     val result = determineWinner()
-    if (result.equals(E)) {
+    if (E == result) {
       println("It's a tie!")
     } else {
       println("The winner is " + result)
     }
   }
 
-  private def determineWinner(): String = {
+  private def determineWinner(): Char = {
     if (determineIfDiagonalIsFilled()) return board(1)(1)
     for (i <- board.indices) {
       if ((board(i) contains X) || (board(i) contains O)) {
@@ -38,19 +38,19 @@ object App {
   }
 
   private def isWinningColumn(i: Int) = {
-    board(0)(i).equals(board(1)(i)) && board(0)(i).equals(board(2)(i))
+    board(0)(i) == board(1)(i) && board(0)(i) == board(2)(i)
   }
 
   private def isWinningRow(i: Int) = {
-    board(i)(0).equals(board(i)(1)) && board(i)(0).equals(board(i)(2))
+    board(i)(0) == board(i)(1) && board(i)(0) == board(i)(2)
   }
 
   private def determineIfDiagonalIsFilled(): Boolean = {
     val middleEntry = board(1)(1)
-    if (middleEntry.equals(X) || middleEntry.equals(O)) {
-      if (board(0)(0).equals(middleEntry) && middleEntry.equals(board(2)(2))) {
+    if (X == middleEntry || middleEntry == O) {
+      if (board(0)(0) == middleEntry && middleEntry == board(2)(2)) {
         return true
-      } else if (board(0)(2).equals(middleEntry) && middleEntry.equals(board(2)(0))) {
+      } else if (board(0)(2) == middleEntry && middleEntry == board(2)(0)) {
         return true
       }
     }
@@ -63,7 +63,7 @@ object App {
     }
   }
 
-  private def printRow(boardRow: Array[String]): Unit = {
+  private def printRow(boardRow: Array[Char]): Unit = {
     print("|")
     boardRow.foreach { boardElem =>
       printEntry(boardElem)
@@ -71,7 +71,7 @@ object App {
     println
   }
 
-  private def printEntry(boardElem: String): Unit = {
+  private def printEntry(boardElem: Char): Unit = {
     print(boardElem + "|")
   }
 }
